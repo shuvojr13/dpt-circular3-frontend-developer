7120e771fa06fb0ae3455ee5c62ae541fe38c89f
shuvojr13 <shuvojr1379@gmail.com>
2024-05-11 19:58:01 +0600

    Project Task Complete

--- src/components/DataCollection.jsx
+++ src/components/DataCollection.jsx
@@ -1,50 +1,72 @@
-import React from 'react';
-import data from '../Data.json';
+import React from "react";
+import data from "../Data.json";
 
 function DataCollection() {
   const { flightOffer } = data;
   const tableHeaders = [
-    'Flight',
-    'Aircraft',
-    'Class',
-    'Fare',
-    'Route',
-    'Departure',
-    'Arrival',
-    'Duration',
-    'Price'
+    "Flight",
+    "Aircraft",
+    "Class",
+    "Fare",
+    "Route",
+    "Departure",
+    "Arrival",
+    "Duration",
+    "Price",
   ];
 
   return (
-    <div className='mx-24'>
-      <p className='p-4 font-medium'>{data.message}</p>
-      <table className='w-full table-fixed  border border-collapse '>
+    <div className="mx-20">
+      <p className="p-4 font-medium">{data.message}</p>
+      <table className="w-full   border border-collapse ">
         <thead>
-        <tr className='p-4 border bg-gray-100'>
+          <tr className="p-4 border bg-gray-100">
             {tableHeaders.map((header, indx) => (
-              <th key={indx} className=' px-4 py-2'>{header}</th>
+              <th key={indx} className=" px-8 py-2">
+                {header}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {flightOffer.map((offer, index) => (
+          {flightOffer.map((offer, index) =>
             offer.itineraries.map((itinerary, idx) => (
               <React.Fragment key={index + idx}>
-              
-                
                 {itinerary.segments.map((segment, i) => (
-                 
-                  <tr key={i} className={i==0 ? 'border-t-2  text-center px-4 ':'text-center px-4'}>
-                    <td >{segment.marketingCarrier}{segment.aircraft} </td>
-                    <td >{segment.flightNumber}</td>
-                    
+                  <tr
+                    key={i}
+                    className={
+                      i == 0
+                        ? "border-t-2  text-center text-sm "
+                        : "text-center text-sm"
+                    }
+                  >
+                    <td>
+                      {segment.marketingCarrier}
+                      {segment.aircraft}{" "}
+                    </td>
+                    <td>{segment.flightNumber}</td>
+                    <td>{offer.class[0][0]}</td>
+                    <td>{offer.fareBasis[0][0]}</td>
+                    <td>
+                      {segment.departure.iataCode} - {segment.arrival.iataCode}
+                    </td>
+                    <td>{segment.departure.at} </td>
+                    <td>{segment.arrival.at} </td>
+                    <td>{i == 0 ? itinerary.duration : null} </td>
+                    <td>
+                      {i === 0 ? (
+                        <React.Fragment>
+                          {offer.price} <br/>
+                          <button className='btn-primary'>Select</button>
+                        </React.Fragment>
+                      ) : null}{" "}
+                    </td>
                   </tr>
-                  
                 ))}
-               
               </React.Fragment>
             ))
-          ))}
+          )}
         </tbody>
       </table>
     </div>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -8,24 +8,24 @@ function Filter() {
           onChange={null}
           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
         />
-        <label>Extra Options</label>
+        <label className="font-bold">Extra Options</label>
       </div>
       <div className="flex flex-row gap-2 items-center">
-        <p>Environment</p>
+        <p className="font-bold">Environment</p>
         <input
           type="radio"
           value=""
           onChange={null}
           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
         />
-        <label>Dummy</label>
+        <label className="font-bold">Dummy</label>
         <input
           type="radio"
           value=""
           onChange={null}
           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
         />
-        <label>PDT</label>
+        <label className="font-bold">PDT</label>
       </div>
       <button className="btn-primary">Search</button>
     </div>

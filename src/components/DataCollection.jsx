d26e1bd3946f05d955f30f764bc3003ddb43ab07
shuvojr13 <shuvojr1379@gmail.com>
2024-05-11 18:38:09 +0600

    Parse data show

--- src/components/DataCollection.jsx
+++ src/components/DataCollection.jsx
@@ -1,41 +1,54 @@
-import data from '../Data.json'; 
+import React from 'react';
+import data from '../Data.json';
 
 function DataCollection() {
-  const {flightOffer} = data ;
+  const { flightOffer } = data;
+  const tableHeaders = [
+    'Flight',
+    'Aircraft',
+    'Class',
+    'Fare',
+    'Route',
+    'Departure',
+    'Arrival',
+    'Duration',
+    'Price'
+  ];
+
   return (
-    <>
-        <div>
-      <h2>Flight Offers</h2>
-      {/* Iterate over the flight offers */}
-      {flightOffer.map((offer, index) => (
-        <div key={index}>
-          <h3>Offer {index + 1}</h3>
-          {/* Iterate over the itineraries */}
-          {offer.itineraries.map((itinerary, idx) => (
-            <div key={idx}>
-              <h4>Itinerary {idx + 1}</h4>
-              {/* Iterate over the segments */}
-              {itinerary.segments.map((segment, i) => (
-                <div key={i}>
-                  <p>
-                    Departure: {segment.departure.iataCode} - {segment.departure.at}
-                  </p>
-                  <p>
-                    Arrival: {segment.arrival.iataCode} - {segment.arrival.at}
-                  </p>
-                  <p>Marketing Carrier: {segment.marketingCarrier}</p>
-                  <p>Carrier Code: {segment.carrierCode}</p>
-                  <p>Flight Number: {segment.flightNumber}</p>
-                  <p>Aircraft: {segment.aircraft}</p>
-                </div>
-              ))}
-            </div>
+    <div className='mx-24'>
+      <p className='p-4 font-medium'>{data.message}</p>
+      <table className='w-full table-fixed  border border-collapse '>
+        <thead>
+        <tr className='p-4 border bg-gray-100'>
+            {tableHeaders.map((header, indx) => (
+              <th key={indx} className=' px-4 py-2'>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {flightOffer.map((offer, index) => (
+            offer.itineraries.map((itinerary, idx) => (
+              <React.Fragment key={index + idx}>
+              
+                
+                {itinerary.segments.map((segment, i) => (
+                 
+                  <tr key={i} className={i==0 ? 'border-t-2  text-center px-4 ':'text-center px-4'}>
+                    <td >{segment.marketingCarrier}{segment.aircraft} </td>
+                    <td >{segment.flightNumber}</td>
+                    
+                  </tr>
+                  
+                ))}
+               
+              </React.Fragment>
+            ))
           ))}
-        </div>
-      ))}
+        </tbody>
+      </table>
     </div>
-    </>
-  )
+  );
 }
 
-export default DataCollection
+export default DataCollection;
